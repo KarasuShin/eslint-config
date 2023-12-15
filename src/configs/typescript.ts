463e74cbd83c05b5909f74caf29d4ee622171eec
Karasu <karasu.shing@gmail.com>
2023-12-15 09:23:17 +0800

    feat(typescript): add recommended rules

--- src/configs/typescript.ts
+++ src/configs/typescript.ts
@@ -80,8 +80,23 @@ export const typescriptConfig: FlatESLintConfig[] = [{
     '@stylistic/plus': pluginStylisticPlus,
   },
   rules: {
+    'constructor-super': 'off', // ts(2335) & ts(2377)
+    'getter-return': 'off', // ts(2378)
     'no-array-constructor': 'off',
-    'no-dupe-class-members': 'off',
+    'no-const-assign': 'off', // ts(2588)
+    'no-dupe-args': 'off', // ts(2300)
+    'no-dupe-class-members': 'off', // ts(2393) & ts(2300)
+    'no-dupe-keys': 'off', // ts(1117)
+    'no-func-assign': 'off', // ts(2630)
+    'no-import-assign': 'off', // ts(2632) & ts(2540)
+    'no-new-symbol': 'off', // ts(7009)
+    'no-obj-calls': 'off', // ts(2349)
+    'no-redeclare': 'off', // ts(2451)
+    'no-setter-return': 'off', // ts(2408)
+    'no-this-before-super': 'off', // ts(2376) & ts(17009)
+    'no-undef': 'off', // ts(2304) & ts(2552)
+    'no-unreachable': 'off', // ts(7027)
+    'no-unsafe-negation': 'off', // ts(2365) & ts(2322) & ts(2358)
     'no-use-before-define': 'off',
     'no-useless-constructor': 'off',
     '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
